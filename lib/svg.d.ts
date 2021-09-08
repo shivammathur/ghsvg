8e43e491cf15637a7a50ddedbe6855d4f138a599
Shivam Mathur <shivam_jpr@hotmail.com>
2021-09-08 08:58:29 +0530

    Add eslint-import-resolver-typescript

--- lib/svg.d.ts
+++ lib/svg.d.ts
@@ -1,5 +1,12 @@
 /// <reference types="node" />
 export declare function compareDeps(a: string, b: string): number;
-export declare function createSVG(data: Record<string, any>[], ghsOptions: Record<string, any>): Promise<string>;
+export declare function createSVG(
+  data: Record<string, any>[],
+  ghsOptions: Record<string, any>
+): Promise<string>;
 export declare function getBase64Image(data: Buffer, format: string): string;
-export declare function getAvatar(avatarUrl: string, avatarCache: string, format: string): Promise<string>;
+export declare function getAvatar(
+  avatarUrl: string,
+  avatarCache: string,
+  format: string
+): Promise<string>;
